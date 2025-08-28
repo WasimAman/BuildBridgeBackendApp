00bc4f3250d6cacea482602b97b16248b6138120
WasimAman <wasimaman65@gmail.com>
2025-08-28 17:57:29 +0530

    Initail commit

---
+++ src/test/java/com/wasim/buildbridge/BuildbridgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wasim.buildbridge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildbridgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
